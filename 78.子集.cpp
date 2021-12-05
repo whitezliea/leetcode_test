f53116122b4e86e505f7450d0a181ac6e7d0ef1d
whitezliea <whitealiez@outlook.com>
2021-12-05 23:39:01 +0800

    回溯算法

---
+++ 78.子集.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=cpp
+ *
+ * [78] 子集
+ */
+
+// @lc code=start
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> res;
+  
+    vector<vector<int>> subsets(vector<int>& nums) {
+        //0的含义是一开始选0个数
+        vector<int> temp;
+        backtrack(nums,0,temp);
+        return res;
+    }
+
+    void backtrack(vector<int>& nums,int k,vector<int>& temp) 
+    {
+        res.push_back(temp);
+        for (int i = k;i<nums.size();i++)
+        {
+                //做选择
+                temp.push_back(nums[i]);
+                //递归下一层
+                //这里错误将i+1写成k+1
+                backtrack(nums,i+1,temp);
+                //取消选择
+                temp.pop_back();
+           
+        }
+    }
+};
+// @lc code=end
+
