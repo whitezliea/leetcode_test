556fda2ebf4693c4b833e8403be91c12f7ab6985
whitezliea <whitealiez@outlook.com>
2021-12-08 22:41:21 +0800

    DFS/回溯算法

--- 黑白无向图.cpp
+++ 黑白无向图.cpp
@@ -21,7 +21,7 @@ v e
 input:
 7 9 1 2 1 4 2 3 2 5 2 6 3 7 4 5 4 6 6 7 
 output:
-1 3 5 6 2 4 7 2
+1 3 5 6 
 */
 #include<iostream>
 #include<stdlib.h>
@@ -58,13 +58,17 @@ void backtrack(int k)
     {
        count += 1;
        //cout<<"方法"<<count<<" :";
-       for (int i=1;i<=v;i++)
-       {
-           //输出白棋的的索引
-           if(used[i]==1)
-           {
-               cout<<i<<" ";
-           }
+       //只打印第一种方案
+       if (count == 1)
+        {
+            for (int i=1;i<=v;i++)
+            {
+                //输出白棋的的索引
+                if(used[i]==1)
+                {
+                    cout<<i<<" ";
+                }
+            }
        }
        //cout<<endl;
        return;
@@ -93,16 +97,17 @@ int main()
         //表示两点是连通的 ,无向图
         edge[e1][e2]= edge[e2][e1] = 1;
     }
-
+    cout<<"图的邻接矩阵为"<<endl;
     for (int i=1;i<=e;i++)
     {
         for (int j = 1; j <= e; j++)
         {
-            cout<<edge[i][j]; 
+            cout<<edge[i][j]<<" "; 
         }
         cout<<endl;
     }
     //从节点1开始递归
+    cout<<"白色点编号为 ："<<endl;
     backtrack(1);
-    cout<<count<<endl;
+    cout<<endl;
 }
