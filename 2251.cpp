37638819f2423f2439d25a33903ee87d1683d832
whitezliea <whitealiez@outlook.com>
2021-12-30 23:16:26 +0800

    DFS

---
+++ 2251.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+char Dun[30][30][30]={'.'};
+bool vis[30][30][30]={false};
+#define MAXSIZE 100
+//高 长 宽
+int r=0,l=0,c=0;
+int sx=0,sy=0,sz=0;
+struct Node
+{
+    int x=0;
+    int y=0;
+    int z=0;
+    int step=0;
+};
+
+
+class Queue
+{
+private:
+    struct Node data[MAXSIZE];
+    int rear;
+    int front;
+    int size;
+public:
+    Queue()
+    {
+        rear=-1;
+        front =-1;
+        size=0;
+    }
+    void add(Node a)
+    {
+        if (isFull())
+            exit(-1);
+        rear ++ ;
+        rear %= MAXSIZE;
+        size++;
+        data[rear] = a;
+    }
+    Node del()
+    {
+        if (isEmpty())
+            exit(-1);
+        front++;
+        front %= MAXSIZE;
+        size--;
+        return data[front];
+    }
+    Node getFront()
+    {
+        int index = front +1;
+        return data[index];
+    }
+    int getSize()
+    {
+        return size;
+    }
+    bool isEmpty()
+    {
+        return size == 0;
+    }
+    bool isFull()
+    {
+        return size == MAXSIZE;
+    }
+    ~Queue();
+};
+
+
+
+void BFS()
+{
+
+}
+
+int main()
+{
+    while (1)
+    {
+        cin>>r>>l>>c;
+        for (int i = 0; i < r; i++)
+        {
+            for (int  j = 0; j < l; j++)
+            {
+                for (int k = 0; i < c; k++)
+                {
+                    cin>>Dun[i][j][k];
+                    if (Dun[i][j][k]=='S')
+                    {
+                        sz=i;
+                        sx=j;
+                        sy=k;
+                    }
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+}
---
+++ poj1321.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+char map[8][8] = { '.' };
+//n是棋盘规格，k是棋子数量
+int n = 0, k = 0;
+int nums = 0;
+
+int DFS(int col,int m)
+{
+    //棋子都放完
+    if (m == k)
+    {
+        return 1;
+    }
+    //限制在一行进行操作，一行一行进行操作
+    int t = n - k + m + 1,res=0;
+    for (int i = col; i < t; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (map[i][j] == '#')
+            {
+                map[i][j] = '.';
+                res += DFS(i + 1, m + 1) ;
+                map[i][j] = '#';
+               
+            }
+        }
+    }
+    return res;
+}
+
+int main()
+{
+
+    while (1)
+    {
+        //初始化
+        nums = 0;
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++)
+                map[i][j] = '.';
+        cin >> n >> k;
+        if (n == -1 && k == -1)
+            break;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                cin >> map[i][j];  
+        int t =DFS(0,0);
+        //cout << "tesy" << endl;
+        
+        cout << t << endl;
+    }
+    return 0;
+}
+
+/*
+#是棋盘区域,.是空白区域
+Sample Input
+2 1
+#.
+.#
+4 4
+...#
+..#.
+.#..
+#...
+5 2
+....#
+...#.
+..#..
+.#...
+#....
+-1 -1
+
+
+Sample Output
+2
+1
+5
+*/
