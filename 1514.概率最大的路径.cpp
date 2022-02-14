b69747bfab480e8f5ac927aa85f13d6fb1a8b703
whitezliea <whitealiez@outlook.com>
2022-02-14 23:59:22 +0800

    dijkstar/BFS

---
+++ 1514.概率最大的路径.cpp
@@ -0,0 +1,119 @@
+
+/*
+ * @lc app=leetcode.cn id=1514 lang=cpp
+ *
+ * [1514] 概率最大的路径
+ */
+
+ // @lc code=start
+#include<vector>
+#include<queue>
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int id = 0;
+    double cost = 0;
+    friend bool operator < (Node a, Node b)    
+    {    
+        return a.cost < b.cost;    //重载大于号使得大的先出队列    
+    }  
+};
+
+class Solution {
+public:
+
+    double dijkstar(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end, double dist[],double **maps)
+    {
+        Node src;
+        src.id = start;
+        src.cost = 1;
+        // base case，start 到 start 的概率就是 1
+        dist[start] = 1;
+
+        priority_queue<Node> q;
+        q.push(src);
+        while (!q.empty())
+        {
+            Node cur = q.top();
+            q.pop();
+            int cur_id = cur.id;
+            double cur_cost = cur.cost;
+            // 遇到终点提前返回
+            if (cur_id == end) 
+            {
+                return cur_cost;
+            }
+        
+            if (cur_cost < dist[cur_id]) {
+                // 已经有一条概率更大的路径到达 curNode 节点了
+                continue;
+            }            
+            for (int i = 0; i < n; i++)
+            {
+                for  (int j = 0;j<n; j++)
+                {
+                    if (cur_id == i && maps[i][j]>=0)
+                    {
+                        //cout << "test " << endl;
+                        int next_id = j;
+                        double weight = maps[i][j];
+                        double dist_nextnode = dist[cur_id] * weight;
+                        if (dist[next_id] < dist_nextnode)
+                        {
+                            dist[next_id] = dist_nextnode;
+                            Node nexts;
+                            nexts.id = next_id;
+                            nexts.cost = dist_nextnode;
+                            q.push(nexts);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return 0.0;
+    }
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        //首先定义一个dist数组
+        double* dist = new double[n];
+        //初始化
+        for (int i = 0; i < n; i++)
+        {
+            dist[i] = -1;
+        }
+
+        double** maps = new double* [n];
+        for (int i = 0; i < n; i++)
+        {
+            maps[i] = new double[n];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                maps[i][j] = -1;
+            }
+            
+        }
+
+
+        for (int i = 0; i < edges.size(); i++)
+        {
+            int from = edges[i][0];
+            int to = edges[i][1];
+            double weight = succProb[i];
+            maps[from][to] = weight;
+            maps[to][from] = weight;
+        }
+
+        //return 0.0;
+        return dijkstar(n, edges, succProb, start, end, dist, maps);
+    }
+};
+// @lc code=end
+//运行超时，需要将邻接矩阵换成邻接表
+
+
