351ed2848c2249d79eb02b1d82a2067a5b8dfc8e
whitezliea <whitealiez@outlook.com>
2022-03-07 21:00:37 +0800

    update

---
+++ Samsung/1048.最长字符串链.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1048 lang=cpp
+ *
+ * [1048] 最长字符串链
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+
+    }
+};
+// @lc code=end
+
---
+++ Samsung/762.二进制表示中质数个计算置位.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=762 lang=cpp
+ *
+ * [762] 二进制表示中质数个计算置位
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+
+    }
+};
+// @lc code=end
+
